36bc0840a9f55b42d73b67ba83e4bd39c84a2954
Lucas Santos <lucasvinicius.bs03@gmail.com>
2022-01-19 18:49:37 -0300

    Create the array based Stack class

---
+++ data-structures/stack-array.js
@@ -0,0 +1,53 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
+    }
+}
+
+const stack = new Stack();
+console.log(stack.isEmpty());
+
+stack.push(5);
+stack.push(8);
+console.log(stack.peek());
+
+stack.push(11);
+console.log(stack.size());
+console.log(stack.isEmpty());
+
+stack.push(15);
+
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
+
+console.log(stack.size());
