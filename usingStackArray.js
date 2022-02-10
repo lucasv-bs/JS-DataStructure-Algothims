339452c07bf0ccc0449677071ed93cf2d778ce41
Lucas Santos <lucasvinicius.bs03@gmail.com>
2022-02-10 08:53:38 -0300

    Added the usingStackArray.js

---
+++ usingStackArray.js
@@ -0,0 +1,23 @@
+const stack = new Stack();
+console.log(stack.isEmpty());
+
+stack.push(5);
+stack.push(8);
+console.log(stack.peek());
+
+stack.push(11);
+console.log(stack.size());
+console.log(stack.isEmpty());
+
+stack.push(15);
+
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
+
+console.log(stack.size());
